0542d896f67596f31e567632a2622abe17bfaae5
Dilip Dalton <ddalton@cloudera.com>
2025-07-23 14:26:55 -0700

    Spdk c function integration

--- spdk-csi-driver/build.rs
+++ spdk-csi-driver/build.rs
@@ -108,8 +108,8 @@ fn build_spdk_bindings() {
         println!("cargo:rustc-link-lib={}", lib);
     }
     
-    // ISA-L library (built as part of SPDK submodules)
-    println!("cargo:rustc-link-lib=dylib=isal");
+    // ISA-L library (built as part of SPDK submodules) - use static linking
+    println!("cargo:rustc-link-lib=static=isal");
     
     // Generate bindings
     let bindings = bindgen::Builder::default()
