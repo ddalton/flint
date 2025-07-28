a72ad1d3490689c6730bf03937844008c1ac8ba1
Dilip Dalton <ddalton@cloudera.com>
2025-07-28 15:42:21 -0700

    Dockerfile changes

--- spdk_flint/include/logging.hpp
+++ spdk_flint/include/logging.hpp
@@ -21,61 +21,61 @@ private:
 };
 
 // Core logging macros
-#define LOG_TRACE(...) spdk_flint::Logger::get()->trace(__VA_ARGS__)
-#define LOG_DEBUG(...) spdk_flint::Logger::get()->debug(__VA_ARGS__)
-#define LOG_INFO(...) spdk_flint::Logger::get()->info(__VA_ARGS__)
-#define LOG_WARN(...) spdk_flint::Logger::get()->warn(__VA_ARGS__)
-#define LOG_ERROR(...) spdk_flint::Logger::get()->error(__VA_ARGS__)
-#define LOG_CRITICAL(...) spdk_flint::Logger::get()->critical(__VA_ARGS__)
+#define LOG_TRACE(...) ::spdk_flint::Logger::get()->trace(__VA_ARGS__)
+#define LOG_DEBUG(...) ::spdk_flint::Logger::get()->debug(__VA_ARGS__)
+#define LOG_INFO(...) ::spdk_flint::Logger::get()->info(__VA_ARGS__)
+#define LOG_WARN(...) ::spdk_flint::Logger::get()->warn(__VA_ARGS__)
+#define LOG_ERROR(...) ::spdk_flint::Logger::get()->error(__VA_ARGS__)
+#define LOG_CRITICAL(...) ::spdk_flint::Logger::get()->critical(__VA_ARGS__)
 
 // Component-specific logging macros
 #define LOG_CSI_INFO(...) \
-    spdk_flint::Logger::get()->info("[CSI] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->info("[CSI] " __VA_ARGS__)
 
 #define LOG_CSI_ERROR(...) \
-    spdk_flint::Logger::get()->error("[CSI] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->error("[CSI] " __VA_ARGS__)
 
 #define LOG_CONTROLLER_INFO(...) \
-    spdk_flint::Logger::get()->info("[CONTROLLER] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->info("[CONTROLLER] " __VA_ARGS__)
 
 #define LOG_CONTROLLER_ERROR(...) \
-    spdk_flint::Logger::get()->error("[CONTROLLER] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->error("[CONTROLLER] " __VA_ARGS__)
 
 #define LOG_CONTROLLER_DEBUG(...) \
-    spdk_flint::Logger::get()->debug("[CONTROLLER] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->debug("[CONTROLLER] " __VA_ARGS__)
 
 #define LOG_SPDK_INFO(...) \
-    spdk_flint::Logger::get()->info("[SPDK] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->info("[SPDK] " __VA_ARGS__)
 
 #define LOG_SPDK_WARN(...) \
-    spdk_flint::Logger::get()->warn("[SPDK] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->warn("[SPDK] " __VA_ARGS__)
 
 #define LOG_SPDK_ERROR(...) \
-    spdk_flint::Logger::get()->error("[SPDK] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->error("[SPDK] " __VA_ARGS__)
 
 #define LOG_DASHBOARD_INFO(...) \
-    spdk_flint::Logger::get()->info("[DASHBOARD] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->info("[DASHBOARD] " __VA_ARGS__)
 
 #define LOG_DASHBOARD_ERROR(...) \
-    spdk_flint::Logger::get()->error("[DASHBOARD] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->error("[DASHBOARD] " __VA_ARGS__)
 
 #define LOG_NODE_AGENT_INFO(...) \
-    spdk_flint::Logger::get()->info("[NODE_AGENT] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->info("[NODE_AGENT] " __VA_ARGS__)
 
 #define LOG_NODE_AGENT_DEBUG(...) \
-    spdk_flint::Logger::get()->debug("[NODE_AGENT] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->debug("[NODE_AGENT] " __VA_ARGS__)
 
 #define LOG_NODE_AGENT_ERROR(...) \
-    spdk_flint::Logger::get()->error("[NODE_AGENT] " __VA_ARGS__)
+    ::spdk_flint::Logger::get()->error("[NODE_AGENT] " __VA_ARGS__)
 
 // RPC operation logging (to replace the Rust RPC logging)
 #define LOG_RPC_CALL(method, ...) \
-    spdk_flint::Logger::get()->debug("[RPC] Calling SPDK method: {}{}", method, ##__VA_ARGS__)
+    ::spdk_flint::Logger::get()->debug("[RPC] Calling SPDK method: {}{}", method, ##__VA_ARGS__)
 
 #define LOG_RPC_SUCCESS(...) \
-    spdk_flint::Logger::get()->debug(__VA_ARGS__)
+    ::spdk_flint::Logger::get()->debug(__VA_ARGS__)
 
 #define LOG_RPC_ERROR(...) \
-    spdk_flint::Logger::get()->error(__VA_ARGS__)
+    ::spdk_flint::Logger::get()->error(__VA_ARGS__)
 
 } // namespace spdk_flint 
